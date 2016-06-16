096c6f945c2b598e0fd4c4df37cbd43ca152439e
Ryan Fowler <ryanfowler88@gmail.com>
2016-06-15 20:41:27 -0500

    Completed v1.0 of Tapper example App for iOS Course

--- Tapper/ViewController.swift
+++ Tapper/ViewController.swift
@@ -10,16 +10,67 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    //Variables
+    var maxTaps: Int = 0
+    var currentTaps: Int = 0
+    
+    //Outlets
+    @IBOutlet weak var imgLogo: UIImageView!
+    @IBOutlet weak var txtTap: UITextField!
+    @IBOutlet weak var btnPlay: UIButton!
+
+    @IBOutlet weak var btnCoin: UIButton!
+    @IBOutlet weak var lblTaps: UILabel!
+    
+    @IBAction func on_btnCoinTapped(sender: UIButton) {
+        currentTaps += 1
+        updatelblTaps()
+        
+        if isGameOver() {
+            restartGame()
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func on_btnPlayPressed(sender: UIButton!) {
+        
+        if txtTap.text != nil && txtTap.text != "" {
+            imgLogo.hidden = true
+            btnPlay.hidden = true
+            txtTap.hidden = true
+            
+            btnCoin.hidden = false
+            lblTaps.hidden = false
+            
+            maxTaps = Int(txtTap.text!)!
+            currentTaps = 0
+            
+            updatelblTaps()
+            
+        }
     }
-
-
+    
+    func restartGame() {
+        maxTaps = 0
+        txtTap.text = ""
+        
+        imgLogo.hidden = false
+        btnPlay.hidden = false
+        txtTap.hidden = false
+        
+        btnCoin.hidden = true
+        lblTaps.hidden = true
+    }
+    
+    func isGameOver() -> Bool {
+        if currentTaps >= maxTaps {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func updatelblTaps() {
+        lblTaps.text = "\(currentTaps) Taps"
+    }
 }
 
